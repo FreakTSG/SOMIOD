8faea20cb0c2085f4ef00596c6f40b7e6b36ded4
André Silva <2201724@my.ipleiria.pt>
2023-11-30 16:31:14 +0000

    database query

---
+++ SOMIOD-IS/App_Data/SQLQuery1.sql
@@ -0,0 +1,31 @@
+﻿CREATE TABLE [dbo].[Data] (
+    [Id]          INT            IDENTITY (1, 1) NOT NULL,
+	[Name] NVARCHAR (50) NOT NULL,
+    [Content]     NVARCHAR (500) NOT NULL,
+    [Creation_dt] NVARCHAR (50)  NOT NULL,
+    [Parent]      INT            NOT NULL,
+  
+);
+CREATE TABLE [dbo].[Container]
+(
+	[Id]          INT           IDENTITY (1, 1) NOT NULL,
+    [Name]        NVARCHAR (50) NOT NULL,
+    [Creation_dt] NVARCHAR (50) NOT NULL,
+    [Parent]      INT           NOT NULL,
+);
+CREATE TABLE [dbo].[Subscription]
+(
+	[Id]          INT           IDENTITY (1, 1) NOT NULL,
+    [Name]        NVARCHAR (50) NOT NULL,
+    [Creation_dt] NVARCHAR (50) NOT NULL,
+    [Parent]      INT           NOT NULL,
+    [Event]       NVARCHAR (50) NOT NULL,
+    [Endpoint]    NVARCHAR (50) NOT NULL,
+);
+CREATE TABLE [dbo].[Application]
+(
+	[Id]          INT           IDENTITY (1, 1) NOT NULL,
+    [Name] NVARCHAR(50) NOT NULL, 
+    [Creation_dt] NVARCHAR(50) NOT NULL
+)
+
