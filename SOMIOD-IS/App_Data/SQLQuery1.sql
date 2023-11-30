a9c6afce84539b5748e495c7fcb5fc1651864325
André Silva <2201724@my.ipleiria.pt>
2023-11-30 17:00:22 +0000

    remove hidden characters

--- SOMIOD-IS/App_Data/SQLQuery1.sql
+++ SOMIOD-IS/App_Data/SQLQuery1.sql
@@ -1,39 +1,38 @@
-﻿﻿CREATE TABLE [dbo].[Application] (
-    [Id]           INT          IDENTITY(1,1) NOT NULL,
-    [Name]         VARCHAR (50) NOT NULL UNIQUE,
-    [CreationDate] DATETIME     NOT NULL,
+﻿CREATE TABLE [dbo].[Application] (
+    [Id] INT IDENTITY(1,1) NOT NULL,
+    [Name] VARCHAR(50) NOT NULL UNIQUE,
+    [CreationDate] DATETIME NOT NULL,
     PRIMARY KEY CLUSTERED ([Id] ASC)
 );
 
 CREATE TABLE [dbo].[Container] (
-    [Id]           INT          IDENTITY(1,1) NOT NULL,
-    [Name]         VARCHAR (50) NOT NULL UNIQUE,
-    [CreationDate] DATETIME     NOT NULL,
-    [Parent]       INT          NOT NULL,
+    [Id] INT IDENTITY(1,1) NOT NULL,
+    [Name] VARCHAR(50) NOT NULL UNIQUE,
+    [CreationDate] DATETIME NOT NULL,
+    [Parent] INT NOT NULL,
     PRIMARY KEY CLUSTERED ([Id] ASC),
-    FOREIGN KEY ([Parent]) REFERENCES [dbo].[Application] ([Id]),
-	CONSTRAINT uq_container UNIQUE([Parent], [Name])
+    FOREIGN KEY ([Parent]) REFERENCES [dbo].[Application]([Id]),
+    CONSTRAINT uq_container UNIQUE([Parent], [Name])
 );
 
 CREATE TABLE [dbo].[Subscription] (
-    [Id]           INT          IDENTITY(1,1) NOT NULL,
-    [Name]         VARCHAR (50) NOT NULL,
-    [CreationDate] DATETIME     NOT NULL,
-    [Parent]       INT          NOT NULL,
-    [Event]        VARCHAR (50) NOT NULL,
-    [Endpoint]     VARCHAR (50) NOT NULL,
+    [Id] INT IDENTITY(1,1) NOT NULL,
+    [Name] VARCHAR(50) NOT NULL,
+    [CreationDate] DATETIME NOT NULL,
+    [Parent] INT NOT NULL,
+    [Event] VARCHAR(50) NOT NULL,
+    [Endpoint] VARCHAR(50) NOT NULL,
     PRIMARY KEY CLUSTERED ([Id] ASC),
-    FOREIGN KEY ([Parent]) REFERENCES [dbo].[Container] ([Id]),
-	CONSTRAINT uq_subscription UNIQUE([Parent], [Name])
+    FOREIGN KEY ([Parent]) REFERENCES [dbo].[Container]([Id]),
+    CONSTRAINT uq_subscription UNIQUE([Parent], [Name])
 );
 
 CREATE TABLE [dbo].[Data] (
-    [Id]           INT          IDENTITY(1,1) NOT NULL,
-    [Name]         VARCHAR (50) NOT NULL,
-    [Content]      VARCHAR (50) NOT NULL,
-    [CreationDate] DATETIME     NOT NULL,
-    [Parent]       INT          NOT NULL,
+    [Id] INT IDENTITY(1,1) NOT NULL,
+    [Name] VARCHAR(50) NOT NULL,
+    [Content] VARCHAR(50) NOT NULL,
+    [CreationDate] DATETIME NOT NULL,
+    [Parent] INT NOT NULL,
     PRIMARY KEY CLUSTERED ([Id] ASC),
-    FOREIGN KEY ([Parent]) REFERENCES [dbo].[Container] ([Id])
+    FOREIGN KEY ([Parent]) REFERENCES [dbo].[Container]([Id])
 );
-
